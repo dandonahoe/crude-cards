7988518f7d193c8ba24ff9943738e2b16e12e897
Daniel Donahoe <dan@construct.works>
2024-08-25 09:24:56 -0400

    feat(imports): reorganize and add new imports
    - feat(imports): new Mantine styles
      - .storybook/preview.tsx
    
    - feat(imports): reorganize existing imports
      - .storybook/preview.tsx
    
    Lines Added: 6
    
    feat(readme): update project information
    
    - feat(readme): add introduction and overview
      - README.md
    
    - feat(readme): describe game overview and mechanics
      - README.md
    
    - feat(readme): highlight educational purpose
      - README.md
    
    Lines Added: Significant
    
    Note: No breaking changes.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,16 @@
 import { GameTemplate } from '../src/ui/game/GameTemplate/index';
 import { AppScript } from '../src/pages/AppContent/AppScript';
+import '@mantine/code-highlight/styles.css';
 import { AppProvider } from '../src/client/AppProvider';
 import { AppTheme } from '../src/client/AppTheme';
 import type { Preview } from "@storybook/react";
 import { MantineProvider } from '@mantine/core';
+import '@mantine/notifications/styles.css';
+import '@mantine/spotlight/styles.css';
+import '@mantine/tiptap/styles.css';
+import '@mantine/dates/styles.css';
 import React from "react";
+import '@mantine/core/styles.css';
 
 const preview: Preview = {
     decorators: [
