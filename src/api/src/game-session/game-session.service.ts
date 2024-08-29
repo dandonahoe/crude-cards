b4e88b605fa2f167a3ef98966724ac1531fb3350
Daniel Donahoe <dan@construct.works>
2024-08-29 13:09:17 -0400

    feat(refactor): update
    - feat(refactor): Refactor getJoinGameScenario method
      - game-session.service.ts
    - feat(simplification): Simplify conditional statements in getJoinGameScenario
      - game-session.service.ts
    - feat(switch): Simplify switch case statements
      - game-session.service.ts
    
    No breaking changes.

--- src/api/src/game-session/game-session.service.ts
+++ src/api/src/game-session/game-session.service.ts
@@ -54,166 +54,138 @@ export class GameSessionService {
     };
 
 
-private getJoinGameScenario = (
-    player: Player, session: GameSession,
-) : JoinGameScenario => {
-
-    const {
-        disconnected_player_id_list,
-        limbo_player_id_list,
-        player_id_list,
-    } = session;
-
-    // NEW GAME AND PLAYER
-    // Joined Pregame Lobby
-    // IF: The game is in lobby mode still before the game has started.
-    // ACTION: Add player to the player_id_list and emit the
-    // updated session to all players in the session.
-    if (session.game_stage === GameStage.Lobby && !player_id_list.includes(player.id)) {
-        return JoinGameScenario.NewGameAndPlayer;
-    }
-
-    // PLAYER FAST REFRESH
-    // Fast Reconnect / Page Refresh
-    // IF: The player is reconnecting and the game doesn't realize they were even
-    // gone. Means disconnect didnt' go through as expected (hence the possible
-    // reason for the disconnect) they they rapidly joined back before any timers
-    // moved them into disconnected state. Means they exist in the player_id_list,
-    // but not in the disconnected_player_id_list or limbo_player_id_list
-    // ACTION: Check the game session and ensure their player_id is only in
-    // the player_id_list and not in the disconnected_player_id_list or limbo_player_id_list
-    // and the game should continue as if nothing happened from other players
-    // perspective. The current user may have just refreshed their browser or lost
-    // connection, server crash whatever the case may be. Should broadcase to all
-    // players (though ther may be an efficiency gain by skipping other players,
-    // nothing should have changed for them)
-    if (
-        player_id_list.includes(player.id) &&
-        !disconnected_player_id_list.includes(player.id) &&
-        !limbo_player_id_list.includes(player.id)
-    ) {
-        return JoinGameScenario.PlayerFastRefresh;
-    }
-
-    // Joining Player is Already in Limbo
-    // IF: If they are listed in the limbo_player_id_list and
-    // are NOT in disconnected_player_id_list, then they're
-    // joinged as a new player while the game is already in progress and
-    // were put into limbo previously. This could happen if they
-    // were in limbo and refreshed the page or rejoined the game multiple
-    // times as the same user
-    // ACTION: do nothing. Could happen if they are in limbo and refresh,
-    // they should just stay there. Emit update to players
-    if (
-        limbo_player_id_list.includes(player.id) &&
-        !disconnected_player_id_list.includes(player.id)
-    ) {
-        return JoinGameScenario.JoiningPlayerIsAlreadyInLimbo;
-    }
-
-    // Reconnecting Disconnected Player
-    // IF: They are listed in the disconnected_player_id_list, then
-    // they were disconnected and the server properly registerd the
-    // disconnnect, and the players were notified with updated state
-    // reflecting the dicsconnected player.
-    // ACTION: Remove them from the disconnected_player_id_list.
-    // The player_id_list has all players, so just removing it from
-    // disconnected reconnectes them to the session. Joining players
-    // who were previously disconnected properly should be
-    // added back automatically. They skip limbo since they're
-    // already known to be in the game and are dealt in.
-    if (disconnected_player_id_list.includes(player.id)) {
-        return JoinGameScenario.ReconnectingDisconnectedPlayer;
-    }
-
-    // PLAYER IS ALREADY IN GAME
-    // IF: The player joining is already in this game and
-    // their player_id is NOT in disconnected_player_id_list
-    // AND NOT in limbo_player_id_list. So they're just an active
-    // player already but a joing game request was sent.
-    // ACTION: Do nothing, they are already in the game so noop.
-    // Emit update to players, but possibly not necessary.
-    if (
-        player_id_list.includes(player.id) &&
-        !disconnected_player_id_list.includes(player.id) &&
-        !limbo_player_id_list.includes(player.id)
-    ) {
-        return JoinGameScenario.PlayerIsAlreadyInGame;
-    }
-
-    // PLAYER JOINS MIDGAME
-    // IF: The new player is unknown to the current game session and
-    // the game has already started (no longer in lobby mode). They are
-    // joining late and have not been dealt in yet.
-    // ACTION: Add their player_id to the limbo_player_id_list
-    // and emit the updated session to all players in the session.
-    // Once the current hand ends, they will be dealt in. Later
-    // on, it will have "Admit / Ignore / Ban" etc options to
-    // allow players to optionally let players in limbo into the game [idea].
-    if (
-        !player_id_list.includes(player.id) &&
-        session.game_stage !== GameStage.Lobby
-    ) {
-        return JoinGameScenario.PlayerJoinsMidGame;
-    }
-
-    // Additional Cases
-    // 1. Player Attempts to Join a Full Game
-    // 2. Player Attempts to Join a Banned Session
-
-    // Default case if none of the above match
-    return JoinGameScenario.JoiningPlayerIsAlreadyInLimbo;
-}
-
-
     private getJoinGameScenario = (
         player: Player, session: GameSession,
     ) : JoinGameScenario => {
 
         const {
-            disconnected_player_id_list, player_id_list, limbo_player_id_list
+            disconnected_player_id_list,
+            limbo_player_id_list,
+            player_id_list,
         } = session;
 
+        // NEW GAME AND PLAYER
+        // Joined Pregame Lobby
+        // IF: The game is in lobby mode still before the game has started.
+        // ACTION: Add player to the player_id_list and emit the
+        // updated session to all players in the session.
+        if (session.game_stage === GameStage.Lobby &&
+            !player_id_list.includes(player.id)) {
+            return JoinGameScenario.NewGameAndPlayer;
+        }
 
+        // PLAYER FAST REFRESH
+        // Fast Reconnect / Page Refresh
+        // IF: The player is reconnecting and the game doesn't realize they were even
+        // gone. Means disconnect didnt' go through as expected (hence the possible
+        // reason for the disconnect) they they rapidly joined back before any timers
+        // moved them into disconnected state. Means they exist in the player_id_list,
+        // but not in the disconnected_player_id_list or limbo_player_id_list
+        // ACTION: Check the game session and ensure their player_id is only in
+        // the player_id_list and not in the disconnected_player_id_list or limbo_player_id_list
+        // and the game should continue as if nothing happened from other players
+        // perspective. The current user may have just refreshed their browser or lost
+        // connection, server crash whatever the case may be. Should broadcase to all
+        // players (though ther may be an efficiency gain by skipping other players,
+        // nothing should have changed for them)
+        if (player_id_list.includes(player.id) &&
+            !disconnected_player_id_list.includes(player.id) &&
+            !limbo_player_id_list.includes(player.id)
+        ) {
+            return JoinGameScenario.PlayerFastRefresh;
+        }
 
+        // Joining Player is Already in Limbo
+        // IF: If they are listed in the limbo_player_id_list and
+        // are NOT in disconnected_player_id_list, then they're
+        // joinged as a new player while the game is already in progress and
+        // were put into limbo previously. This could happen if they
+        // were in limbo and refreshed the page or rejoined the game multiple
+        // times as the same user
+        // ACTION: do nothing. Could happen if they are in limbo and refresh,
+        // they should just stay there. Emit update to players
+        if (limbo_player_id_list.includes(player.id) &&
+            !disconnected_player_id_list.includes(player.id)
+        ) {
+            return JoinGameScenario.JoiningPlayerIsAlreadyInLimbo;
+        }
 
+        // Reconnecting Disconnected Player
+        // IF: They are listed in the disconnected_player_id_list, then
+        // they were disconnected and the server properly registerd the
+        // disconnnect, and the players were notified with updated state
+        // reflecting the dicsconnected player.
+        // ACTION: Remove them from the disconnected_player_id_list.
+        // The player_id_list has all players, so just removing it from
+        // disconnected reconnectes them to the session. Joining players
+        // who were previously disconnected properly should be
+        // added back automatically. They skip limbo since they're
+        // already known to be in the game and are dealt in.
+        if (disconnected_player_id_list.includes(player.id)) {
+            return JoinGameScenario.ReconnectingDisconnectedPlayer;
+        }
 
+        // PLAYER IS ALREADY IN GAME
+        // IF: The player joining is already in this game and
+        // their player_id is NOT in disconnected_player_id_list
+        // AND NOT in limbo_player_id_list. So they're just an active
+        // player already but a joing game request was sent.
+        // ACTION: Do nothing, they are already in the game so noop.
+        // Emit update to players, but possibly not necessary.
+        if (player_id_list.includes(player.id) &&
+            !disconnected_player_id_list.includes(player.id) &&
+            !limbo_player_id_list.includes(player.id)
+        ) {
+            return JoinGameScenario.PlayerIsAlreadyInGame;
+        }
 
-
-
-
-
-
-
+        // PLAYER JOINS MIDGAME
+        // IF: The new player is unknown to the current game session and
+        // the game has already started (no longer in lobby mode). They are
+        // joining late and have not been dealt in yet.
+        // ACTION: Add their player_id to the limbo_player_id_list
+        // and emit the updated session to all players in the session.
+        // Once the current hand ends, they will be dealt in. Later
+        // on, it will have "Admit / Ignore / Ban" etc options to
+        // allow players to optionally let players in limbo into the game [idea].
+        if (!player_id_list.includes(player.id) &&
+            session.game_stage !== GameStage.Lobby
+        ) {
+            return JoinGameScenario.PlayerJoinsMidGame;
+        }
 
         // Additional Cases
         // 1. Player Attempts to Join a Full Game
         // 2. Player Attempts to Join a Banned Session
 
+        // Default case if none of the above match
         return JoinGameScenario.JoiningPlayerIsAlreadyInLimbo;
-
-    };
+    }
 
     public setPlayerGameSession = async (
         player: Player, session: GameSession,
-    ) => {
+    ) : P<void> => {
         this.log.silly('GameSessionService::addPlayerToSession', { player, session });
+
+        const stageAndId = {
+            game_stage : Not(GameStage.GameComplete),
+            id         : Not(session.id),
+        };
+
+        const playerIdArray = ArrayContains([player.id]);
 
         // Find all active game sessions this player is tied to and
         // remove them, except for the session we're adding outselves to.
         const activeGameSessionList = await this.gameSessionRepo.find({
             where : [{
-                player_id_list : ArrayContains([player.id]),
-                game_stage     : Not(GameStage.GameComplete),
-                id             : Not(session.id),
+                disconnected_player_id_list : playerIdArray,
+                ...stageAndId,
             }, {
-                disconnected_player_id_list : ArrayContains([player.id]),
-                game_stage                  : Not(GameStage.GameComplete),
-                id                          : Not(session.id),
+                limbo_player_id_list : playerIdArray,
+                ...stageAndId,
             }, {
-                limbo_player_id_list : ArrayContains([player.id]),
-                game_stage           : Not(GameStage.GameComplete),
-                id                   : Not(session.id),
+                player_id_list : playerIdArray,
+                ...stageAndId,
             }],
         });
 
@@ -224,55 +196,32 @@ private getJoinGameScenario = (
                 async activeSession =>
                     this.removePlayer(player, activeSession)));
 
-        // now update the session we're joining.
-        // A few states can occur here:
-
-        // choose cases which eliminate the most states first
         const joinGameState = this.getJoinGameScenario(player, session);
 
         switch(joinGameState) {
-            case JoinGameScenario.ReconnectingDisconnectedPlayer:
-                this.joinGameViaReconnectingDisconnectedPlayer();
-                break;
+            case JoinGameScenario.ReconnectingDisconnectedPlayer: this.joinGameViaReconnectingDisconnectedPlayer(); break;
+            case JoinGameScenario.JoiningPlayerIsAlreadyInLimbo : this.joinGameViaJoiningPlayerIsAlreadyInLimbo();  break;
+            case JoinGameScenario.PlayerIsAlreadyInGame         : this.joinGameViaPlayerIsAlreadyInGame();          break;
+            case JoinGameScenario.PlayerJoinsMidGame            : this.joinGameViaPlayerJoinsMidGame();             break;
+            case JoinGameScenario.PlayerFastRefresh             : this.joinGameViaPlayerFastRefresh();              break;
+            case JoinGameScenario.NewGameAndPlayer              : this.joinGameViaNewGameAndPlayer();               break;
 
-            case JoinGameScenario.JoiningPlayerIsAlreadyInLimbo :
-                this.joinGameViaJoiningPlayerIsAlreadyInLimbo();
-                break;
-
-            case JoinGameScenario.PlayerIsAlreadyInGame         :
-                this.joinGameViaPlayerIsAlreadyInGame();
-                break;
-
-            case JoinGameScenario.PlayerJoinsMidGame            :
-                this.joinGameViaPlayerJoinsMidGame();
-                break;
-
-            case JoinGameScenario.PlayerFastRefresh             :
-                this.joinGameViaPlayerFastRefresh();
-                break;
-
-            case JoinGameScenario.NewGameAndPlayer              :
-                this.joinGameViaNewGameAndPlayer();
-                break;
-
-            default:
-                throw new WsException(`Invalid Join Game Scenario ${joinGameState}`);
+            default: throw new WsException(`Invalid Join Game Scenario ${joinGameState}`);
         }
-
-        // The game is in the lobby and hasnt started, auto admit them
-        return this.gameSessionRepo
-            .createQueryBuilder()
-            .update(GameSession)
-            .set({
-                player_id_list : () => `array_append(player_id_list, '${player.id}')`,
-            })
-            .where("id = :id", { id : session.id })
-            .andWhere("game_stage != :game_stage", {
-                game_stage : GameStage.GameComplete,
-            })
-            .execute();
     }
 
+    // // The game is in the lobby and hasnt started, auto admit them
+        // return this.gameSessionRepo
+        //     .createQueryBuilder()
+        //     .update(GameSession)
+        //     .set({
+        //         player_id_list : () => `array_append(player_id_list, '${player.id}')`,
+        //     })
+        //     .where("id = :id", { id : session.id })
+        //     .andWhere("game_stage != :game_stage", {
+        //         game_stage : GameStage.GameComplete,
+        //     })
+        //     .execute();
     private joinGameViaReconnectingDisconnectedPlayer =  async () : P<void> => {
 
     };
