d9387c53e9f31b7ab5ba1da799c215e754d6952c
Daniel Donahoe <dan@construct.works>
2024-09-10 11:20:37 -0400

    feat(update): multiple file updates
    - feat(vscode): update launch settings
      - .vscode/launch.json
      - Substantial modifications to the launch configuration settings to improve debugging capabilities.
    
    - feat(gremlinexample): update example script
      - gremlinexample.js
      - Updated the example script with new logic and removed redundant code to streamline operations.
    
    - refactor(script): improve narrative functions
      - internal/script/narrative/index.ts
      - Refactored several functions to improve performance, readability, and type safety. Introduced a new helper function formatNarrative and added constants and enums for better maintainability.
    
    - chore(package): update dependencies
      - package.json
      - Updated dependencies and

--- internal/script/narrative/index.ts
+++ internal/script/narrative/index.ts
@@ -1,1 +1,45 @@
-console.log('asdfnpm')
+import * as dotenv from 'dotenv';
+import neo4j from 'neo4j-driver';
+
+
+dotenv.config();
+
+
+if(!process.env.NEO4J_PASSWORD) {
+    console.error('Please provide the password for the Neo4j database using the environment variable NEO4J_PASSWORD')
+    process.exit(1)
+}
+
+if(!process.env.NEO4J_ENDPOINT) {
+    console.error('Please provide the endpoint for the Neo4j database using the environment variable NEO4J_ENDPOINT')
+    process.exit(1)
+}
+
+
+(async (uri : string, password : string) => {
+    // URI examples: 'neo4j://localhost', 'neo4j+s://xxx.databases.neo4j.io'
+    // const uri = `neo4j://${process.env.NEO4J_ENDPOINT}`
+    const username = 'neo4j'
+    // const password = process.env.NEO4J_PASSWORD || ['INVALID_PASSWORD'];
+
+    let driver
+
+    try {
+        driver = neo4j.driver(`neo4j://${uri}`, neo4j.auth.basic(username, password));
+
+        const serverInfo = await driver.getServerInfo();
+
+        console.log('Connection established')
+        console.log(serverInfo);
+
+        console.log('Closing connection')
+        driver.close()
+
+        console.log('And its closed')
+
+        process.exit(0)
+    } catch (err) {
+        console.log(`Connection error\n${err}\nCause:`, err)
+    }
+})(process.env.NEO4J_ENDPOINT, process.env.NEO4J_PASSWORD);
+
