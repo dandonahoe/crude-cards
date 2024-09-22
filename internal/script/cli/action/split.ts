2bcd5990c3dc6997ac366184dbd6e1f7288f1376
Dan Donahoe <daniel.donahoe@gmail.com>
2024-09-21 20:52:02 -0400

    feat(cli): cli testing works

---
+++ internal/script/cli/action/split.ts
@@ -0,0 +1,27 @@
+import inquirer from 'inquirer';
+
+export const splitAction = async () => {
+  // Prompting the user for input using Inquirer
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'stringToSplit',
+      message: 'Enter the string you want to split:',
+    },
+    {
+      type: 'input',
+      name: 'separator',
+      message: 'Enter the separator character (default is ","):',
+      default: ',',
+    },
+    {
+      type: 'confirm',
+      name: 'firstOnly',
+      message: 'Display only the first substring?',
+      default: false,
+    },
+  ]);
+
+  const limit = answers.firstOnly ? 1 : undefined;
+  console.log(answers.stringToSplit.split(answers.separator, limit));
+};
--- internal/script/cli/index.ts
+++ internal/script/cli/index.ts
@@ -1,40 +1,16 @@
 import { Command } from '@commander-js/extra-typings';
-import inquirer from 'inquirer';
+import { splitAction } from './action/split';
 
 const program = new Command();
 
 program
-  .name('string-util')
-  .description('CLI to some JavaScript string utilities')
-  .version('0.8.0');
+    .name('pew')
+    .description('CLI to some JavaScript string utilities')
+    .version('0.8.0');
 
 program
-  .command('split')
-  .description('Split a string into substrings and display as an array')
-  .action(async () => {
-    // Prompting the user for input using Inquirer
-    const answers = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'stringToSplit',
-        message: 'Enter the string you want to split:',
-      },
-      {
-        type: 'input',
-        name: 'separator',
-        message: 'Enter the separator character (default is ","):',
-        default: ',',
-      },
-      {
-        type: 'confirm',
-        name: 'firstOnly',
-        message: 'Display only the first substring?',
-        default: false,
-      },
-    ]);
-
-    const limit = answers.firstOnly ? 1 : undefined;
-    console.log(answers.stringToSplit.split(answers.separator, limit));
-  });
+    .command('split')
+    .description('Split a string into substrings and display as an array')
+    .action(splitAction);
 
 program.parse();
