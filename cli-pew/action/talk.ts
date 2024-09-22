ec66e480f3583a8f5a93623cbad6f75662c6364a
Dan Donahoe <daniel.donahoe@gmail.com>
2024-09-21 22:21:16 -0400

    feat(cli): adding data area

--- cli-pew/action/talk.ts
+++ cli-pew/action/talk.ts
@@ -1,27 +1,13 @@
-import inquirer from 'inquirer';
+import { createClient } from "@deepgram/sdk";
+import * as dotenv from 'dotenv';
+// - or -
+// const { createClient } = require("@deepgram/sdk");
+
+dotenv.config();
+
+const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
 
 export const talkAction = async () => {
-    // Prompting the user for input using Inquirer
-    const answers = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'stringToSplit',
-            message: 'Enter the string you want to split:',
-        },
-        {
-            type: 'input',
-            name: 'separator',
-            message: 'Enter the separator character (default is ","):',
-            default: ',',
-        },
-        {
-            type: 'confirm',
-            name: 'firstOnly',
-            message: 'Display only the first substring?',
-            default: false,
-        },
-    ]);
 
-    const limit = answers.firstOnly ? 1 : undefined;
-    console.log(answers.stringToSplit.split(answers.separator, limit));
+    console.log('Talk action', deepgram.version);
 };
