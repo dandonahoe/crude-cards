5ddf02a5995ed3187c68904fa98357ad6f107c03
Dan Donahoe <daniel.donahoe@gmail.com>
2024-09-22 10:02:15 -0400

    feat(cli): cli testing works

--- src/ui/game/GameButton/GameButton.stories.tsx
+++ src/ui/game/GameButton/GameButton.stories.tsx
@@ -2,19 +2,11 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 import { GameButton } from '.';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-    title      : 'Game/GameButton',
-    component  : GameButton,
-    parameters : {
-        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-        layout : 'centered',
-    },
-    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-    tags     : ['autodocs'],
-    // More on argTypes: https://storybook.js.org/docs/api/argtypes
-    argTypes : {
-
+    title     : 'Game/GameButton',
+    component : GameButton,
+    tags      : ['autodocs'],
+    argTypes  : {
     },
 
     args : {
@@ -25,8 +17,6 @@ const meta = {
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
     args : {
 
--- src/ui/game/GameButton/index.tsx
+++ src/ui/game/GameButton/index.tsx
@@ -1,30 +1,22 @@
+import { Button, rem, Text } from '@mantine/core';
 import classes from './GameButton.module.css';
-import { Button, Text } from '@mantine/core';
 import { RFC } from '@app/ui/type';
 import { Props } from './type';
 
 
-export const GameButton : RFC<Props> = ({
+export const GameButton: RFC<Props> = ({
     text, onClick,
-}) => {
-    const buttonSize = 180;
-    const fontSize   = 'xl';
-
-    return (
-        <Button
-            style={{
-                width  : buttonSize,
-                height : buttonSize,
-            }}
-            aria-label={`Button ${text}`}
-            className={classes.gameButton}
-            onClick={onClick}>
-            <Text
-                fz={fontSize}
-                fw={800}
-                lh={0.5}>
-                {text}
-            </Text>
-        </Button>
-    );
-}
+}) =>
+    <Button
+        className={classes.gameButton}
+        aria-label={`Button ${text}`}
+        onClick={onClick}
+        w={rem(180)}
+        h={rem(180)}>
+        <Text
+            fw={600}
+            lh={0.5}
+            fz='xl'>
+            {text}
+        </Text>
+    </Button>
