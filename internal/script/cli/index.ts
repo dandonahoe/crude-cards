a4e51556b222562a40029e5f55000d4ae6b1d288
Dan Donahoe <daniel.donahoe@gmail.com>
2024-09-21 20:44:08 -0400

    feat(cli): cli testing works

--- internal/script/cli/index.ts
+++ internal/script/cli/index.ts
@@ -1,20 +1,40 @@
 import { Command } from '@commander-js/extra-typings';
+import inquirer from 'inquirer';
 
 const program = new Command();
 
 program
-    .name('string-util')
-    .description('CLI to some JavaScript string utilities')
-    .version('0.8.0');
+  .name('string-util')
+  .description('CLI to some JavaScript string utilities')
+  .version('0.8.0');
 
-program.command('split')
-    .description('Split a string into substrings and display as an array')
-    .argument('<string>', 'string to split')
-    .option('--first', 'display just the first substring')
-    .option('-s, --separator <char>', 'separator character', ',')
-    .action((str : string, options) => {
-        const limit = options.first ? 1 : undefined;
-        console.log(str.split(options.separator, limit));
-    });
+program
+  .command('split')
+  .description('Split a string into substrings and display as an array')
+  .action(async () => {
+    // Prompting the user for input using Inquirer
+    const answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'stringToSplit',
+        message: 'Enter the string you want to split:',
+      },
+      {
+        type: 'input',
+        name: 'separator',
+        message: 'Enter the separator character (default is ","):',
+        default: ',',
+      },
+      {
+        type: 'confirm',
+        name: 'firstOnly',
+        message: 'Display only the first substring?',
+        default: false,
+      },
+    ]);
+
+    const limit = answers.firstOnly ? 1 : undefined;
+    console.log(answers.stringToSplit.split(answers.separator, limit));
+  });
 
 program.parse();
