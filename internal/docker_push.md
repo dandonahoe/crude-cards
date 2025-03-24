849b7e09fdd192d945551beb38f464f2de8b275b
Dan Donahoe <daniel.donahoe@gmail.com>
2025-03-23 21:28:16 -0400

    feat(ci): added build and push docker script

--- internal/docker_push.md
+++ internal/docker_push.md
@@ -1,7 +1,21 @@
+# Building and Pushing Docker Images
+
+-   **Authenticate**
+
+`aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 938413686327.dkr.ecr.us-east-1.amazonaws.com`
+
+-   **Tag the local 'crude-cards/api' image with the ECR repository URI**
+
+```bash
 docker tag crude-cards/api:latest 938413686327.dkr.ecr.us-east-1.amazonaws.com/crude-cards/api:latest
 docker tag crude-cards/web:latest 938413686327.dkr.ecr.us-east-1.amazonaws.com/crude-cards/web:latest
 docker tag crude-cards/ui:latest 938413686327.dkr.ecr.us-east-1.amazonaws.com/crude-cards/ui:latest
+```
 
+-   **Push the 'crude-cards/api' image to the ECR repository**
+
+```bash
 docker push 938413686327.dkr.ecr.us-east-1.amazonaws.com/crude-cards/api:latest
 docker push 938413686327.dkr.ecr.us-east-1.amazonaws.com/crude-cards/web:latest
 docker push 938413686327.dkr.ecr.us-east-1.amazonaws.com/crude-cards/ui:latest
+```
--- internal/script/docker.ts
+++ internal/script/docker.ts
@@ -1,25 +1,45 @@
-console.log('This script is not implemented yet. ass.');
+#!/usr/bin/env zx
 
-// import { exec } from 'child_process';
+import { $ } from 'zx';
 
-// const commands = [
-//     'docker build --progress=plain --no-cache -f DockerfileUI -t crude-cards-ui-container .',
-//     'docker build --progress=plain --no-cache -f DockerfileAPI -t rude-cards-api-container .',
-//     'docker build --progress=plain --no-cache -f DockerfileWeb -t rude-cards-web-container .',
-// ];
+// docker tag crude-cards/api:latest 938413686327.dkr.ecr.us-east-1.amazonaws.com/crude-cards/api:latest
+// docker tag crude-cards/web:latest 938413686327.dkr.ecr.us-east-1.amazonaws.com/crude-cards/web:latest
+// docker tag crude-cards/ui:latest 938413686327.dkr.ecr.us-east-1.amazonaws.com/crude-cards/ui:latest
 
-// commands.forEach(command => {
-//     const process = exec(command);
+// docker push 938413686327.dkr.ecr.us-east-1.amazonaws.com/crude-cards/api:latest
+// docker push 938413686327.dkr.ecr.us-east-1.amazonaws.com/crude-cards/web:latest
+// docker push 938413686327.dkr.ecr.us-east-1.amazonaws.com/crude-cards/ui:latest
 
-//     process.stdout?.on('data', data => {
-//         console.log(`[stdout] ${data}`);
-//     });
+const main = async () => {
 
-//     process.stderr?.on('data', data => {
-//         console.error(`[stderr] ${data}`);
-//     });
+    const registry = '938413686327.dkr.ecr.us-east-1.amazonaws.com';
+    const images   = ['api', 'web', 'ui'];
+    const repo     = 'crude-cards';
 
-//     process.on('close', code => {
-//         console.log(`Command "${command}" exited with code ${code}`);
-//     });
-// });
+    console.log('Logging into ECR...')
+
+    // eslint-disable-next-line max-len
+    await $`aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 938413686327.dkr.ecr.us-east-1.amazonaws.com`.pipe(process.stdout);
+
+    console.log('Building Docker Images...')
+
+    await $`IS_BUILDING=true COMPOSE_BAKE=true DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 docker compose build`.pipe(process.stdout);
+
+    await Promise.all(
+        images.map(async name => {
+
+            const fullTag = `${registry}/${repo}/${name}:latest`;
+
+            console.log(`🔖 Tagging ${name}...`);
+            await $`docker tag ${repo}/${name}:latest ${fullTag}`
+
+            console.log(`📤 Pushing ${name}...`);
+            await $`docker push ${fullTag}`
+        }),
+    );
+}
+
+main().catch(err => {
+    console.error('❌ Failed:', err);
+    process.exit(1);
+});
